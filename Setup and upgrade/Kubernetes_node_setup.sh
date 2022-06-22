b2e3fac0346c1af176b221f8ffca403cdfe320fd
Suyesh Singh <40593135+Suyeshsingh@users.noreply.github.com>
2022-06-22 11:12:26 +0545

    Update Kubernetes_node_setup.sh

--- Setup and upgrade/Kubernetes_node_setup.sh
+++ Setup and upgrade/Kubernetes_node_setup.sh
@@ -50,4 +50,4 @@ echo "Installing Kubernetes"
   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 }
 
-apt update && apt install -y kubeadm=1.16.2-00 kubelet=1.16.2-00 kubectl=1.16.2-00
+apt update && apt install -y kubeadm=1.22.2-00 kubelet=1.22.2-00 kubectl=1.22.2-00
